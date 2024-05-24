2a4aa394de5754913b15e55c32ce5dcf1b8b2964
JTepjuk <Jaturon.tepjuk@pexa.com.au>
2024-05-24 17:38:16 +1000

    test list

--- src/main/kotlin/com/example/filemaker_spike/controller/CustomerController.kt
+++ src/main/kotlin/com/example/filemaker_spike/controller/CustomerController.kt
@@ -3,15 +3,12 @@ package com.example.filemaker_spike.controller
 import com.example.filemaker_spike.service.CustomerService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
 
 @Controller
 class CustomerController(val customerService: CustomerService) {
 
     @GetMapping("/test")
-//    fun index(@ModelAttribute("model") model: ModelMap): String {
     fun index(model: Model): String {
         model.addAttribute("customers", customerService.getTitleDetails())
         return "index"
